11fe34d14d3cf80a83745f6a2168a83ee7b3d4aa
SOUHTYYY <leshka.l1337@gmail.com>
2021-11-21 19:14:33 +0300

    refactor: ready to production

--- main.js
+++ main.js
@@ -16,45 +16,16 @@ switch (process.platform) {
 }
 
 console.log("DIRNAME", __dirname);
+console.log(process.env.NODE_ENV);
+// process.env.NODE_ENV === "production" ? `../${pluginName}` : pluginName;
+
 app.commandLine.appendSwitch(
   "ppapi-flash-path",
-  path.join(
-    __dirname,
-    process.env.NODE_ENV === "production" ? `../${pluginName}` : pluginName
-  )
+  path.join(__dirname, `../${pluginName}`)
 );
-app.setName("y8-browser");
+app.setName("gamcore-browser");
 // Optional: Specify flash version, for example, v17.0.0.169
 app.commandLine.appendSwitch("ppapi-flash-version", "11.2.999");
-
-const openDefaultBrowser = async (link) => {
-  await shell.openExternal(link);
-};
-
-const MENU_TEMPLATE = [
-  {
-    label: "Window Manager",
-    submenu: [
-      {
-        label: "Go to website",
-        click: () => openDefaultBrowser("https://porcore.com"),
-      },
-    ],
-  },
-  {
-    label: "Porncore",
-    submenu: [
-      {
-        label: "Go to website",
-        click: () => openDefaultBrowser("https://porcore.com"),
-      },
-    ],
-  },
-];
-
-const menu = Menu.buildFromTemplate(MENU_TEMPLATE);
-
-Menu.setApplicationMenu(menu);
 
 app.on("ready", () => {
   let win = new BrowserWindow({
@@ -64,5 +35,87 @@ app.on("ready", () => {
       plugins: true,
     },
   });
-  win.loadURL(`https://ru.gamcore.com`);
+
+  const openDefaultBrowser = async (link) => {
+    await shell.openExternal(link);
+  };
+
+  const MENU_TEMPLATE = [
+    {
+      label: "Gamcore",
+      submenu: [
+        {
+          label: "Homepage",
+          click: function () {
+            win.loadURL("https://gamcore.com");
+          },
+        },
+        {
+          label: "Quit",
+          accelerator: "Command+Q",
+          click: function () {
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:",
+        },
+      ],
+    },
+    {
+      label: "Screen",
+      submenu: [
+        {
+          label: "Full Screen",
+          accelerator: `${
+            process.platform === "darwin" ? "Cmd+Ctrl+f" : "Ctrl+f11"
+          }`,
+          click: () => toggleFullscreen(),
+        },
+      ],
+    },
+    {
+      label: "Porn Websites",
+      submenu: [
+        {
+          label: "Gamcore",
+          click: () =>
+            openDefaultBrowser("https://gamcore.com/?utm_source=browser"),
+        },
+        {
+          label: "Porcore",
+          click: () =>
+            openDefaultBrowser("https://porcore.com/?utm_source=browser"),
+        },
+      ],
+    },
+  ];
+
+  function toggleFullscreen() {
+    if (win.isFullScreen()) {
+      win.setFullScreen(false);
+    } else {
+      win.setFullScreen(true);
+    }
+  }
+
+  win.maximize();
+  win.loadURL(`https://gamcore.com`);
+
+  const menu = Menu.buildFromTemplate(MENU_TEMPLATE);
+
+  Menu.setApplicationMenu(menu);
 });
